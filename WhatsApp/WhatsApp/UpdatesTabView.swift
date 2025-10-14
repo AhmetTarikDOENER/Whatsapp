a5656099c919a6c8671a52023ce953d75170da8f
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2025-10-14 15:51:10 +0300

    Added StatusSectionHeader to UpdatesTabView. Decided to go with micro structures/extracted while creating sections and its title instead of creating all UI component in a body directly.

--- WhatsApp/WhatsApp/UpdatesTabView.swift
+++ WhatsApp/WhatsApp/UpdatesTabView.swift
@@ -1,13 +1,33 @@
 import SwiftUI
 
+private struct StatusSectionHeader: View {
+    var body: some View {
+        HStack(alignment: .top) {
+            Image(systemName: "circle.dashed")
+                .foregroundStyle(.blue)
+                .imageScale(.large)
+            
+            VStack {
+                Text("Use status to share photos, text and videos that disappear in 24 hours.") + Text("\n")
+                + Text("Status privacy")
+                    .foregroundStyle(.blue).bold()
+            }
+            .padding(.horizontal)
+            
+            Image(systemName: "xmark")
+                .foregroundStyle(.gray)
+        }
+    }
+}
+
 struct UpdatesTabView: View {
     
     @State private var searchText = ""
-
+    
     var body: some View {
         NavigationStack {
             List {
-                
+                StatusSectionHeader()
             }
             .navigationTitle("Updates")
             .searchable(text: $searchText)
