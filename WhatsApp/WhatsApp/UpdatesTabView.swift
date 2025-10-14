14ae3f6f740a6f02dd9f81745207293549213372
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2025-10-14 16:09:02 +0300

    Added StatusSection as well to UpdatesTabView. Also created new color set for StatusSectionHeader to be able to support any appearances in light or dark mode.

--- WhatsApp/WhatsApp/UpdatesTabView.swift
+++ WhatsApp/WhatsApp/UpdatesTabView.swift
@@ -17,6 +17,56 @@ private struct StatusSectionHeader: View {
             Image(systemName: "xmark")
                 .foregroundStyle(.gray)
         }
+        .padding()
+        .background(.whatsAppWhite)
+        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+    }
+}
+
+private struct StatusSection: View {
+    var body: some View {
+        HStack {
+            Circle()
+                .frame(width: 60, height: 60)
+            
+            VStack(alignment: .leading) {
+                Text("My Status")
+                    .font(.callout).bold()
+                
+                Text("Add to my status")
+                    .foregroundStyle(.gray)
+                    .font(.system(size: 15))
+            }
+            
+            Spacer()
+            
+            makeCameraButton()
+            makePencilButton()
+        }
+    }
+    
+    private func makeCameraButton() -> some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "camera.fill")
+                .padding(10)
+                .background(Color(.systemGray5))
+                .clipShape(Circle())
+                .bold()
+        }
+    }
+    
+    private func makePencilButton() -> some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "pencil")
+                .padding(10)
+                .background(Color(.systemGray5))
+                .clipShape(Circle())
+                .bold()
+        }
     }
 }
 
@@ -28,7 +78,10 @@ struct UpdatesTabView: View {
         NavigationStack {
             List {
                 StatusSectionHeader()
+                    .listRowBackground(Color.clear)
+                StatusSection()
             }
+            .listStyle(.grouped)
             .navigationTitle("Updates")
             .searchable(text: $searchText)
         }
