f3fc3901e31c51e9eee385215e400259a99f423d
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2025-10-15 12:40:54 +0300

    Started to crate ChatroomView by first adding ToolBarContents.

---
+++ WhatsApp/WhatsApp/Core/Chat/View/ChatroomView.swift
@@ -0,0 +1,70 @@
+import SwiftUI
+
+struct ChatroomView: View {
+    
+    //  MARK: - Body
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0 ..< 12) { _ in
+                    Text("PLACEHOLDER")
+                        .font(.largeTitle)
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 200)
+                        .background(Color.gray.opacity(0.1))
+                }
+            }
+            .toolbar {
+                makeLeadingNavigationItems()
+                makeTrailingNavigationGroupItems()
+            }
+        }
+    }
+}
+
+//  MARK: - ChatroomView+Extension
+extension ChatroomView {
+    @ToolbarContentBuilder
+    private func makeLeadingNavigationItems() -> some ToolbarContent {
+        ToolbarItem(placement: .topBarLeading) {
+            HStack {
+                Circle()
+                    .frame(width: 35, height: 35)
+                
+                Text("johndoe")
+                    .bold()
+            }
+        }
+    }
+    
+    @ToolbarContentBuilder
+    private func makeTrailingNavigationGroupItems() -> some ToolbarContent {
+        ToolbarItemGroup(placement: .topBarTrailing) {
+            makeVideoCallButton()
+            makeVoiceCallButton()
+        }
+    }
+    
+    private func makeVideoCallButton() -> some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "video")
+        }
+    }
+    
+    private func makeVoiceCallButton() -> some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "phone")
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ChatroomView()
+    }
+}
