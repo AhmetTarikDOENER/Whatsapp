f1068665cf25761eaa0a4a2faac90da5761c6ee5
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2025-10-16 21:43:44 +0300

    Added UserService structure which is responsible for paginating and returns User array, the return type is abstracted by UserNode structure.

--- WhatsApp/WhatsApp/Screen/Core/Root/ViewModel/RootViewModel.swift
+++ WhatsApp/WhatsApp/Screen/Core/Root/ViewModel/RootViewModel.swift
@@ -11,17 +11,5 @@ final class RootViewModel: ObservableObject {
             .sink { [weak self] latestAuthState in
                 self?.authState = latestAuthState
             }
-        
-        AuthService.testAccounts.forEach { email in
-            registerTestAccounts(with: email)
-        }
-    }
-    
-    /// Register Test Accounts to Backend
-    private func registerTestAccounts(with email: String) {
-        Task {
-            let username = email.replacingOccurrences(of: "@gmail.com", with: "")
-            try? await AuthService.shared.createAccount(for: username, with: email, and: "123456")
-        }
     }
 }
---
+++ WhatsApp/WhatsApp/Service/UserService.swift
@@ -0,0 +1,35 @@
+import Foundation
+import Firebase
+import FirebaseDatabase
+
+struct UserService {
+    static func paginateUsers(lastCursor: String?, pageSize: UInt) async throws -> UserNode {
+        /// Initial data fetch, there is no lastCursor
+        if lastCursor == nil {
+            let mainSnapshot = try await FirebaseConstants.UserReference.queryLimited(toLast: pageSize).getData()
+            guard let first = mainSnapshot.children.allObjects.first as? DataSnapshot,
+                  let allObjects = mainSnapshot.children.allObjects as? [DataSnapshot] else { return .emptyNode }
+            
+            let users: [User] = allObjects.compactMap { userSnapshot in
+                let userDictionary = userSnapshot.value as? [String: Any] ?? [:]
+                return User(dictionary: userDictionary)
+            }
+            
+            if users.count == mainSnapshot.childrenCount {
+                let userNode = UserNode(users: users, currentCursor: first.key)
+                return userNode
+            }
+            
+            return .emptyNode
+        } else {
+            /// Paginate more data
+            return .emptyNode
+        }
+    }
+}
+
+struct UserNode {
+    var users: [User]
+    var currentCursor: String?
+    static let emptyNode = UserNode(users: [], currentCursor: nil)
+}
