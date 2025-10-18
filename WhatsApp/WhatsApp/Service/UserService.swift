aee75628b55e1f6fe5433535f1ef0f8edd1977db
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2025-10-18 12:25:57 +0300

    REFACTORED: TO avoid code duplication, refactored UserService's paginateUsers(lastCursor:pageSize) static method.

--- WhatsApp/WhatsApp/Service/UserService.swift
+++ WhatsApp/WhatsApp/Service/UserService.swift
@@ -3,48 +3,38 @@ import Firebase
 import FirebaseDatabase
 
 struct UserService {
+    
     static func paginateUsers(lastCursor: String?, pageSize: UInt) async throws -> UserNode {
-        /// Initial data fetch, there is no lastCursor
+        let mainSnapshot: DataSnapshot
         if lastCursor == nil {
-            let mainSnapshot = try await FirebaseConstants.UserReference.queryLimited(toLast: pageSize).getData()
-            guard let first = mainSnapshot.children.allObjects.first as? DataSnapshot,
-                  let allObjects = mainSnapshot.children.allObjects as? [DataSnapshot] else { return .emptyNode }
-            
-            let users: [User] = allObjects.compactMap { userSnapshot in
-                let userDictionary = userSnapshot.value as? [String: Any] ?? [:]
-                return User(dictionary: userDictionary)
-            }
-            
-            if users.count == mainSnapshot.childrenCount {
-                let userNode = UserNode(users: users, currentCursor: first.key)
-                return userNode
-            }
-            
-            return .emptyNode
+            mainSnapshot = try await FirebaseConstants.UserReference
+                .queryLimited(toLast: pageSize)
+                .getData()
         } else {
-            /// Paginate more data
-            let mainSnapshot = try await FirebaseConstants.UserReference
+            mainSnapshot = try await FirebaseConstants.UserReference
                 .queryOrderedByKey()
                 .queryEnding(atValue: lastCursor)
                 .queryLimited(toLast: pageSize + 1)
                 .getData()
-            
-            guard let first = mainSnapshot.children.allObjects.first as? DataSnapshot,
-                  let allObjects = mainSnapshot.children.allObjects as? [DataSnapshot] else { return .emptyNode }
-            
-            let users: [User] = allObjects.compactMap { userSnapshot in
-                let userDictionary = userSnapshot.value as? [String: Any] ?? [:]
-                return User(dictionary: userDictionary)
-            }
-            
-            if users.count == mainSnapshot.childrenCount {
-                let filteredUsers = users.filter { $0.uid != lastCursor }
-                let userNode = UserNode(users: filteredUsers, currentCursor: first.key)
-                return userNode
-            }
-            
-            return .emptyNode
         }
+        
+        guard let first = mainSnapshot.children.allObjects.first as? DataSnapshot,
+              let allObjects = mainSnapshot.children.allObjects as? [DataSnapshot] else { return .emptyNode }
+        
+        let users: [User] = allObjects.compactMap { userSnapshot in
+            let userDictionary = userSnapshot.value as? [String: Any] ?? [:]
+            
+            return User(dictionary: userDictionary)
+        }
+        
+        if users.count == mainSnapshot.childrenCount {
+            let filteredUsers = lastCursor == nil ? users : users.filter { $0.uid != lastCursor }
+            let userNode = UserNode(users: filteredUsers, currentCursor: first.key)
+            
+            return userNode
+        }
+        
+        return .emptyNode
     }
 }
 
